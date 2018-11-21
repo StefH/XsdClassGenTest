498bcbfb2e03b6d808a492fd69d3c1cffbe274d7
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2018-11-21 16:11:21 +0100

    add 2.xsd

--- Domain/XsdClassGen.cs
+++ Domain/XsdClassGen.cs
@@ -205,3 +205,195 @@ namespace MySpecialDomainNamespace {
         }
     }
 }
+namespace MySpecialDomainNamespace {
+    
+    
+    /// <summary>
+    ///   Automatically generated XML type mapping for ConfigType.
+    /// </summary>
+    /// <remarks>
+    ///   This is an automaticaly generated type, please do not edit it.
+    /// </remarks>
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.Xml", "4.7.3056.0")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.Xml.Serialization.XmlTypeAttribute(Namespace="http://xml2cpp.com/config.xsd")]
+    [System.Xml.Serialization.XmlRootAttribute(Namespace="http://xml2cpp.com/config.xsd", IsNullable=true)]
+    public partial class ConfigType {
+        
+        private string userNameField;
+        
+        private int maxRecentProjectsField;
+        
+        private string[] recentProjectField;
+        
+        private WindowType[] childWindowField;
+        
+        private DictionaryFileType dictionaryField;
+        
+        /// <summary>
+        ///   Gets or sets userName.
+        /// </summary>
+        public string userName {
+            get {
+                return this.userNameField;
+            }
+            set {
+                this.userNameField = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Gets or sets maxRecentProjects.
+        /// </summary>
+        public int maxRecentProjects {
+            get {
+                return this.maxRecentProjectsField;
+            }
+            set {
+                this.maxRecentProjectsField = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Gets or sets recentProject.
+        /// </summary>
+        [System.Xml.Serialization.XmlElementAttribute("recentProject")]
+        public string[] recentProject {
+            get {
+                return this.recentProjectField;
+            }
+            set {
+                this.recentProjectField = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Gets or sets childWindow.
+        /// </summary>
+        [System.Xml.Serialization.XmlElementAttribute("childWindow")]
+        public WindowType[] childWindow {
+            get {
+                return this.childWindowField;
+            }
+            set {
+                this.childWindowField = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Gets or sets dictionary.
+        /// </summary>
+        public DictionaryFileType dictionary {
+            get {
+                return this.dictionaryField;
+            }
+            set {
+                this.dictionaryField = value;
+            }
+        }
+    }
+    
+    /// <summary>
+    ///   Automatically generated XML type mapping for WindowType.
+    /// </summary>
+    /// <remarks>
+    ///   This is an automaticaly generated type, please do not edit it.
+    /// </remarks>
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.Xml", "4.7.3056.0")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.Xml.Serialization.XmlTypeAttribute(Namespace="http://xml2cpp.com/config.xsd")]
+    [System.Xml.Serialization.XmlRootAttribute(Namespace="http://xml2cpp.com/config.xsd", IsNullable=true)]
+    public partial class WindowType {
+        
+        private string nameField;
+        
+        private uint widthField;
+        
+        private uint heightField;
+        
+        /// <summary>
+        ///   Gets or sets name.
+        /// </summary>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string name {
+            get {
+                return this.nameField;
+            }
+            set {
+                this.nameField = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Gets or sets width.
+        /// </summary>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public uint width {
+            get {
+                return this.widthField;
+            }
+            set {
+                this.widthField = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Gets or sets height.
+        /// </summary>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public uint height {
+            get {
+                return this.heightField;
+            }
+            set {
+                this.heightField = value;
+            }
+        }
+    }
+    
+    /// <summary>
+    ///   Automatically generated XML type mapping for DictionaryFileType.
+    /// </summary>
+    /// <remarks>
+    ///   This is an automaticaly generated type, please do not edit it.
+    /// </remarks>
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.Xml", "4.7.3056.0")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.Xml.Serialization.XmlTypeAttribute(Namespace="http://xml2cpp.com/config.xsd")]
+    [System.Xml.Serialization.XmlRootAttribute(Namespace="http://xml2cpp.com/config.xsd", IsNullable=true)]
+    public partial class DictionaryFileType {
+        
+        private string languageField;
+        
+        private string valueField;
+        
+        /// <summary>
+        ///   Gets or sets language.
+        /// </summary>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string language {
+            get {
+                return this.languageField;
+            }
+            set {
+                this.languageField = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Gets or sets Value.
+        /// </summary>
+        [System.Xml.Serialization.XmlTextAttribute()]
+        public string Value {
+            get {
+                return this.valueField;
+            }
+            set {
+                this.valueField = value;
+            }
+        }
+    }
+}
